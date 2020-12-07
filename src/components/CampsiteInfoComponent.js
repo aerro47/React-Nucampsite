7e69c99cfee5dd060dc640f3e0826d77d8cd35c6
Tarik Joullanar <aerro47@gmail.com>
2020-12-07 13:53:16 -0600

    Redux Actions

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -23,7 +23,7 @@ function RenderCampsite({campsite}) {
 }
 
 
-function RenderComments({comments}) {
+function RenderComments({comments, addComment, campsiteId}) {
     if (comments) {
         return (
             <div className="col-md-5 m-1">
@@ -40,7 +40,7 @@ function RenderComments({comments}) {
                         );
                     })
                 }
-                <CommentForm />
+                <CommentForm campsiteId={campsiteId} addComment={addComment}/>
             </div>
         );
     }
@@ -57,16 +57,18 @@ class CommentForm extends Component {
         this.toggleModal = this.toggleModal.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
+
     toggleModal() {
         this.setState({
             isModalOpen: !this.state.isModalOpen
         });
     }
+    
     handleSubmit(values) {
         this.toggleModal();
-        console.log('Current State is: ' + JSON.stringify(values));
-        alert('Current State is: ' + JSON.stringify(values));
+        this.props.addComment(this.props.campsiteId, values.rating, values.author, values.text);
     }
+
     render() {
         return (
             <div>
@@ -144,7 +146,11 @@ function CampsiteInfo(props) {
                 </div>
                 <div className="row">
                     <RenderCampsite campsite={props.campsite} />
-                    <RenderComments comments={props.comments} />
+                    <RenderComments 
+                        comments={props.comments}
+                        addComment={props.addComment}
+                        campsiteId={props.campsite.id}
+                        />
                 </div>
             </div>
         );
